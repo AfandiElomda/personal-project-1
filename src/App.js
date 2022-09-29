aa6ff19035b43b284b09d0cf48e581e8dfe0b20d
+ <+>
2022-09-29 08:03:31 +0300

    set the routing structure

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import {Box} from '@mui/material';
 const App = () => (
     <BrowserRouter>
       <Box sx={{backgroundColor: '#000'}}>
-        Navbar
+        <Navbar />
         <Routes>
-          
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/Video/:id" element={<VideoDetail />} />
+          <Route path="/Channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/searchTerm" element={<SearchFeed />} />
         </Routes>
       </Box>
     </BrowserRouter>
