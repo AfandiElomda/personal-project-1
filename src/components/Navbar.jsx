97ea35ea6b8c9a2a3baf7d5e2d5dd8ce1a97f3f0
+ <+>
2022-09-29 08:30:18 +0300

    utility file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+
+const Navbar = () => {
   return (
     <div>Navbar</div>
   )
